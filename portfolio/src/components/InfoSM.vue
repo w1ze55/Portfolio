3db46033ffbb6fbd619f2016739291a099fb076f
w1ze55 <cacauezin@gmail.com>
2025-06-22 23:44:12 -0300

    Feat: started project, added elements

---
+++ portfolio/src/components/InfoSM.vue
@@ -0,0 +1,63 @@
+<script lang="ts">
+export default {
+    name: 'InfoSM',
+    data() {
+        return {
+            name: 'Caue Peres',
+            description: 'Welcome to my portfolio!',
+            title: 'Developer FullStack Jr',
+        }
+    } 
+}
+</script>
+
+<template>
+  
+  <div class="img-logo">
+    <img src="../assets/foto eu.jpg" alt="Foto de perfil">
+  </div>
+    <div class="app">
+        <header class="hero">
+            <h1>{{ name }}</h1>
+            <h2>{{ title }}</h2>
+            <p>{{ description }}</p>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+}
+
+.app {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 2rem;
+}
+
+.hero {
+  text-align: center;
+  margin-bottom: 4rem;
+}
+
+.hero h1 {
+  font-size: 3rem;
+}
+
+.hero h2 {
+  font-size: 1.5rem;
+}
+
+section {
+  margin-bottom: 3rem;
+}
+
+h3 {
+  font-size: 2rem;
+  border-bottom: 2px solid #3498db;
+  padding-bottom: 0.5rem;
+}
+</style>
---
+++ portfolio/src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+ul li {
+    display: inline-block;
+    margin-right: 10px;
+}
+
+ul li a {
+    text-decoration: none;
+    transition: color 0.3s ease;
+}
+
+ul li a:hover {
+    color: #777;
+    transition: color 0.3s ease;
+}
+
+
+</style>
